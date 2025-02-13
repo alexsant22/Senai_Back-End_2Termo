c52833a2a41b4db20251a02b801b2199b18d2752
alexsant22 <alexandre.santana2201@gmail.com>
2025-02-13 16:14:10 -0300

    Created Project

---
+++ javaMVC_semBD/src/main/java/com/example/javaMVC_semBD/JavaMvcSemBdApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaMVC_semBD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMvcSemBdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMvcSemBdApplication.class, args);
+	}
+
+}
---
+++ javaMVC_semBD/src/test/java/com/example/javaMVC_semBD/JavaMvcSemBdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaMVC_semBD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMvcSemBdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
