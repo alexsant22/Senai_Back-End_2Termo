fee7553bb67bdb67a5330f2a009c69863360fcaf
alexsant22 <alexandre.santana2201@gmail.com>
2025-02-13 16:52:27 -0300

    UserBD - Banco

---
+++ javaMVC_semBD/src/main/java/com/example/javaMVC_semBD/banco/UserBD.java
@@ -0,0 +1,51 @@
+package com.example.javaMVC_semBD.banco;
+
+import com.example.javaMVC_semBD.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserBD {
+
+    public List<User> users; // Lista para usuários (simulando BD)
+
+    public UserBD() { // Construtor
+        this.users = new ArrayList<>();
+    }
+
+    // Buscar todos usuários
+    public List<User> findAll(){
+        return new ArrayList<>(users);
+    }
+
+    // Busca apenas um usuário
+    public User getById(Long id) {
+        return users.stream()
+                .filter(user -> user.getId() == id)
+                .findFirst()
+                .orElse(null);
+    }
+
+    // Inserir usuário
+    public boolean inset(User user) {
+        users.add(user);
+        return true;
+    }
+
+    // Update usuário
+    public boolean upadate(Long id, User user) {
+        User userBd = users.stream()
+                .filter(u -> u.getId() == id)
+                .findFirst()
+                .orElse(null);
+
+        if (userBd == null) {
+            return false;
+        }
+
+        userBd.setNome(user.getNome());
+        userBd.setSobrenome(user.getSobrenome());
+
+        return true;
+    }
+}
