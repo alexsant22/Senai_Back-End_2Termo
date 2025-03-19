3a35e40d88dcf102cfe11fb7924fc7b2a87aca65
alexsant22 <alexandre.santana2201@gmail.com>
2025-03-19 14:14:57 -0300

    ExercJPA

---
+++ ExercJPA/src/main/java/com/example/ExercJPA/ExercJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ExercJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercJpaApplication.class, args);
+	}
+
+}
