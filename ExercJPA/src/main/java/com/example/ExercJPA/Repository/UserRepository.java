54f35ed6167073911a56702327e7d1ac668c6b3d
alexsant22 <alexandre.santana2201@gmail.com>
2025-03-19 14:39:43 -0300

    UserRepository - Repository

--- ExercJPA/src/main/java/com/example/ExercJPA/Repository/UserRepository.java
+++ ExercJPA/src/main/java/com/example/ExercJPA/Repository/UserRepository.java
@@ -1,2 +1,7 @@
-package com.example.ExercJPA.Repository;public interface UserRepository {
+package com.example.ExercJPA.Repository;
+
+import com.example.ExercJPA.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
