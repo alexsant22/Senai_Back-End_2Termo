31ca578f6558596b8c0198aa52f1097f0b65adff
alexsant22 <alexandre.santana2201@gmail.com>
2025-03-19 14:48:32 -0300

    UserController - Controller

--- ExercJPA/src/main/java/com/example/ExercJPA/Controller/UserController.java
+++ ExercJPA/src/main/java/com/example/ExercJPA/Controller/UserController.java
@@ -1,4 +1,23 @@
 package com.example.ExercJPA.Controller;
 
+import com.example.ExercJPA.Entity.User;
+import com.example.ExercJPA.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User user) {
+        User userBd = userRepository.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(userBd);
+    }
 }
