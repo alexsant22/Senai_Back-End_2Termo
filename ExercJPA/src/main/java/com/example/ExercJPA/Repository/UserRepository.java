b5c8cd35c601cd9fb9d8e38e26287a250226711c
alexsant22 <alexandre.santana2201@gmail.com>
2025-03-19 17:34:00 -0300

    UserRepository - Repository

--- ExercJPA/src/main/java/com/example/ExercJPA/Repository/UserRepository.java
+++ ExercJPA/src/main/java/com/example/ExercJPA/Repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.example.ExercJPA.Repository;
 import com.example.ExercJPA.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    public List<User> findByNomeAndCargo (String nome, String cargo);
 }
