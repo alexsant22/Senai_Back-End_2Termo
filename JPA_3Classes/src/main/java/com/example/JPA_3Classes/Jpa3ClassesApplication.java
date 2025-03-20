0679413a0667840a1beaffe9695882f0d682ee82
alexsant22 <alexandre.santana2201@gmail.com>
2025-03-20 14:03:45 -0300

    Criando projeto JPA_3Classes

---
+++ JPA_3Classes/src/main/java/com/example/JPA_3Classes/Jpa3ClassesApplication.java
@@ -0,0 +1,13 @@
+package com.example.JPA_3Classes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jpa3ClassesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jpa3ClassesApplication.class, args);
+	}
+
+}
