f96ddad1657a3dde2978a657f9b148223c2056c8
alexsant22 <alexandre.santana2201@gmail.com>
2025-03-26 15:03:15 -0300

    Controller

--- JPA_3Classes/src/main/java/com/example/JPA_3Classes/Controller/ProfessorController.java
+++ JPA_3Classes/src/main/java/com/example/JPA_3Classes/Controller/ProfessorController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/professor")
@@ -15,14 +16,14 @@ public class ProfessorController {
     @Autowired
     private ProfessorRepository professorRepository;
 
-    @PostMapping("/professorAdd")
+    @PostMapping("/adicionar")
     public ResponseEntity<Professor> create(@RequestBody Professor professor) {
         Professor professorBd = professorRepository.save(professor);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(professorBd);
     }
 
-    @GetMapping("/professoresFind")
+    @GetMapping("/buscar")
     public ResponseEntity<List<Professor>> getAll() {
         List<Professor> professorList = professorRepository.findAll();
 
@@ -32,5 +33,45 @@ public class ProfessorController {
         }
         return ResponseEntity.status(HttpStatus.OK)
                 .body(professorList);
+    }
+
+    @GetMapping("/buscar/{id}")
+    public ResponseEntity<Professor> getById(@PathVariable Long id) {
+        Optional<Professor> professorOptional = professorRepository.findById(id);
+        if (professorOptional.isPresent()) {
+            Professor professor = professorOptional.get();
+            return ResponseEntity.ok(professor);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @PutMapping("/atualizar/{id}")
+    public ResponseEntity<Object> update(@PathVariable Long id, @RequestBody Professor professorAtt) {
+        Optional<Professor> optionalProfessor = professorRepository.findById(id);
+
+        if (optionalProfessor.isPresent()) {
+            Professor professor = optionalProfessor.get();
+            professor.setNome(professorAtt.getNome());
+            professor.setCpf(professorAtt.getCpf());
+
+            return ResponseEntity.ok(professorRepository.save(professor));
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Professor não encontrado.");
+        }
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id) {
+        Optional<Professor> professorOptional = professorRepository.findById(id);
+
+        if (professorOptional.isPresent()) {
+            Professor professor = professorOptional.get();
+            professorRepository.delete(professor);
+
+            return ResponseEntity.ok("Professor deletado com sucesso.");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Professor não encontrado.");
+        }
     }
 }
