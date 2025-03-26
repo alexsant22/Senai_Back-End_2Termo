0974664c526c93643cffe4fe88a70f755f754a7d
alexsant22 <alexandre.santana2201@gmail.com>
2025-03-26 17:05:40 -0300

    Service

---
+++ APIs - Com DTO/JPA_3Classes_DTO/src/main/java/com/example/JPA_3Classes/Service/ProfessorService.java
@@ -0,0 +1,26 @@
+package com.example.JPA_3Classes.Service;
+
+import com.example.JPA_3Classes.DTO.ProfessorDTO;
+import com.example.JPA_3Classes.Entity.Professor;
+import com.example.JPA_3Classes.Repository.ProfessorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ProfessorService {
+    @Autowired
+    private ProfessorRepository professorRepository;
+
+    public Professor fromDTO(ProfessorDTO professorDTO) {
+        Professor professor = new Professor();
+        professor.setNome(professorDTO.getNome());
+        professor.setCpf(professorDTO.getCpf());
+
+        return professor;
+    }
+
+    public Professor save(ProfessorDTO professorDTO) {
+        Professor professor = this.fromDTO(professorDTO);
+        Professor professorBd = professorRepository.save(professor);
+
+        return professorBd;
+    }
+}
