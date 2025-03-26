5b0c656109cbe7c358a0aa5afc7132a5fd02f8e9
alexsant22 <alexandre.santana2201@gmail.com>
2025-03-26 17:15:27 -0300

    Controller

--- APIs - Com DTO/JPA_3Classes_DTO/src/main/java/com/example/JPA_3Classes/Controller/AlunoController.java
+++ APIs - Com DTO/JPA_3Classes_DTO/src/main/java/com/example/JPA_3Classes/Controller/AlunoController.java
@@ -13,5 +13,66 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/aluno")
 public class AlunoController {
+    @Autowired
+    private AlunoRepository alunoRepository;
 
+    @PostMapping("/adicionar")
+    public ResponseEntity<Aluno> create(@RequestBody Aluno aluno) {
+        Aluno alunoBd = alunoRepository.save(aluno);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(alunoBd);
+    }
+
+    @GetMapping("/buscar")
+    public ResponseEntity<List<Aluno>> getAll() {
+        List<Aluno> alunoList = alunoRepository.findAll();
+        if (alunoList.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT)
+                    .build();
+        }
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(alunoList);
+    }
+
+    @GetMapping("/buscar/{id}")
+    public ResponseEntity<Aluno> getById(@PathVariable Long id) {
+        Optional<Aluno> optionalAluno = alunoRepository.findById(id);
+
+        if (optionalAluno.isPresent()) {
+            Aluno aluno = optionalAluno.get();
+            return ResponseEntity.ok(aluno);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @PutMapping("/atualizar/{id}")
+    public ResponseEntity<Object> update(@PathVariable Long id, @RequestBody Aluno alunoAtt) {
+        Optional<Aluno> optionalAluno = alunoRepository.findById(id);
+
+        if (optionalAluno.isPresent()) {
+            Aluno aluno = optionalAluno.get();
+            aluno.setNome(alunoAtt.getNome());
+            aluno.setCpf(alunoAtt.getCpf());
+            aluno.setCurso(alunoAtt.getCurso());
+
+            return ResponseEntity.ok(alunoRepository.save(aluno));
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Aluno não encontrado.");
+        }
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id) {
+        Optional<Aluno> optionalAluno = alunoRepository.findById(id);
+
+        if (optionalAluno.isPresent()) {
+            Aluno aluno = optionalAluno.get();
+            alunoRepository.delete(aluno);
+
+            return ResponseEntity.ok("Aluno deletado com sucesso.");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Aluno não encontrado.");
+        }
+    }
 }
