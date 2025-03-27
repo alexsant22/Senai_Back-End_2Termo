18d3c04072c85998b5a0f3314f6bad28b3e6d312
alexsant22 <alexandre.santana2201@gmail.com>
2025-03-27 16:34:38 -0300

    Controller

--- APIs - Com DTO/JPA_3Classes_DTO/src/main/java/com/example/JPA_3Classes/Controller/ProfessorController.java
+++ APIs - Com DTO/JPA_3Classes_DTO/src/main/java/com/example/JPA_3Classes/Controller/ProfessorController.java
@@ -8,6 +8,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/professor")
 public class ProfessorController {
@@ -16,9 +19,44 @@ public class ProfessorController {
     private ProfessorService professorService;
 
     @PostMapping("/adicionar")
-    public ResponseEntity<Professor> created(@RequestBody ProfessorDTO professorDTO) {
-        Professor professorBd = professorService.save(professorDTO);
-
+    public ResponseEntity<ProfessorDTO> created(@RequestBody ProfessorDTO professorDTO) {
+        ProfessorDTO professorBd = professorService.save(professorDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(professorBd);
+    }
+
+    @GetMapping("/buscar")
+    public ResponseEntity<List<Professor>> getAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(professorService.getAll());
+    }
+
+    @GetMapping("/buscar/{id}")
+    public ResponseEntity<ProfessorDTO> getById(@PathVariable Long id) {
+        Optional<ProfessorDTO> professorDTO = professorService.getById(id);
+
+        if (!professorDTO.isEmpty()) {
+            return ResponseEntity.ok(professorDTO.get());
+        } else  {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @PutMapping("/atualizar/{id}")
+    public ResponseEntity<ProfessorDTO> update(@PathVariable Long id, @RequestBody ProfessorDTO professorDTO) {
+        Optional<ProfessorDTO> professorDTOOptional = professorService.updateProfessor(id, professorDTO);
+
+        if (professorDTOOptional.isPresent()) {
+            return ResponseEntity.ok(professorDTOOptional.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        if (professorService.delete(id)) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
