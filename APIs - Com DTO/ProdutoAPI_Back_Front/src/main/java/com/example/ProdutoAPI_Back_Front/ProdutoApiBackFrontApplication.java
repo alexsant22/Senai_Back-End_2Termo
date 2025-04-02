bc8cec0bdd0d5635d433f210e2ee820a6be48d5e
alexsant22 <alexandre.santana2201@gmail.com>
2025-04-02 13:52:15 -0300

    Projeto criado

---
+++ APIs - Com DTO/ProdutoAPI_Back_Front/src/main/java/com/example/ProdutoAPI_Back_Front/ProdutoApiBackFrontApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProdutoAPI_Back_Front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutoApiBackFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutoApiBackFrontApplication.class, args);
+	}
+
+}
---
+++ APIs - Com DTO/ProdutoAPI_Back_Front/src/test/java/com/example/ProdutoAPI_Back_Front/ProdutoApiBackFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProdutoAPI_Back_Front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutoApiBackFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
