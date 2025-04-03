4d68a284a9c1bb1376c0edb1bc6d689a2e8325ba
alexsant22 <alexandre.satana2201@gmail.com>
2025-04-03 15:36:35 -0300

    Service

--- APIs - Com DTO/ProdutoAPI_Back_Front/src/main/java/com/example/ProdutoAPI_Back_Front/Service/ProdutoService.java
+++ APIs - Com DTO/ProdutoAPI_Back_Front/src/main/java/com/example/ProdutoAPI_Back_Front/Service/ProdutoService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProdutoService {
@@ -39,15 +40,54 @@ public class ProdutoService {
         return produtoDTO;
     }
 
-    public List<Produto> getAll() { // Buscar todos
-        return produtoRepository.findAll();
-    }
-
     public ProdutoDTO saveDTO(ProdutoDTO produtoDTO) {
         Produto produto = this.fromDTO(produtoDTO);
 
         Produto produtoBd = produtoRepository.save(produto);
 
         return this.toDTO(produtoBd);
+    }
+
+    public List<Produto> getAll() { // Buscar todos
+        return produtoRepository.findAll();
+    }
+
+
+    public Optional<ProdutoDTO> getById(Long id) {
+        Optional<Produto> optionalProduto = produtoRepository.findById(id);
+
+        if (optionalProduto.isPresent()) {
+            return Optional.of(this.toDTO(optionalProduto.get()));
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public Optional<ProdutoDTO> updateProduto(Long id, ProdutoDTO produtoDTO) {
+        Optional<Produto> optionalProduto = produtoRepository.findById(id);
+
+        if (optionalProduto.isPresent()) {
+            Produto produto = optionalProduto.get();
+
+            produto.setNome(produtoDTO.getNome());
+            produto.setValor(produtoDTO.getValor());
+            produto.setSaldo(produtoDTO.getSaldo());
+            produto.setSaldoMinimo(produtoDTO.getSaldoMinimo());
+
+            Produto produtoAtt = produtoRepository.save(produto);
+
+            return Optional.of(this.toDTO(produtoAtt));
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public boolean delete(Long id) {
+        if (produtoRepository.existsById(id)) {
+            produtoRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
     }
 }
